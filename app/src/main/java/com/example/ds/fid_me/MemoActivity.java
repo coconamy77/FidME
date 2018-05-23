19f88db4749af8260ee9b11b1fa1e6780304200e
A7\DS <coconamy7727@naver.com>
2018-05-23 19:27:59 +0900

    merge1-> layout

--- app/src/main/java/com/example/ds/fid_me/MemoActivity.java
+++ app/src/main/java/com/example/ds/fid_me/MemoActivity.java
@@ -3,13 +3,11 @@ package com.example.ds.fid_me;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.ds.fid_me.R;
-
-public class MainActivity extends AppCompatActivity {
+public class MemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_memo);
     }
 }
