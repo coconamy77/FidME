2ae1ef74c01c60131d7ca4283701df0aee3b4e94
A7\DS <coconamy7727@naver.com>
2018-05-23 16:57:00 +0900

    처음

---
+++ app/src/main/java/com/example/ds/fid_me/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.ds.fid_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+        //try{
+       //     Thread.sleep(1000);
+      //  }catch (Exception e){};
+
+       // setContentView(R.layout.activity_main);
+    }
+}
