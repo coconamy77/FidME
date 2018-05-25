a6083d84857d8c849118615a09dee3fd60f34348
B4\DS <coconamy7727@naver.com>
2018-05-25 11:55:12 +0900

    layout생성=-> ui생성

--- app/src/main/java/com/example/ds/fid_me/HistoryActivity.java
+++ app/src/main/java/com/example/ds/fid_me/HistoryActivity.java
@@ -3,11 +3,11 @@ package com.example.ds.fid_me;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class BookMarkActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_mark);
+        setContentView(R.layout.activity_history);
     }
 }
