9a0f2cf691f38aca14499722deacd077de48a156
B4\DS <coconamy7727@naver.com>
2018-06-01 10:46:12 +0900

    no message

---
+++ app/src/main/java/com/example/ds/fid_me/SQLiteHelper.java
@@ -0,0 +1,32 @@
+package com.example.ds.fid_me;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by DS on 2018-06-01.
+ */
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    public SQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void queryData(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    public void insertData(String table, )
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
